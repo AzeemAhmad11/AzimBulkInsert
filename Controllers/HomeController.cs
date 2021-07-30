f7547d98878af552d9cc0f05dd41fd2d11cf295c
o876 <o876@DESKTOP-93AGF24>
2021-07-30 22:14:07 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using BulkInsert.Models;
+using EFCore.BulkExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkInsert.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public readonly ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var TotalEmp = _db.Employee.Count();
+            ViewBag.TotalEmployee = TotalEmp;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+            List<Employee> employee = new List<Employee>();
+            employee = GetDataforInsert();
+            _db.BulkInsert(employee);
+            return RedirectToAction("Index");
+        }
+
+        public static List<Employee> GetDataforInsert()
+        {
+            List<Employee> employee = new List<Employee>();
+            for (int i = 0; i < 100000; i++)
+            {
+                employee.Add(new Employee()
+                {
+                    Name="Employee_" +i,
+                    Department="Department_" +i,
+                    City = "City_" +i
+                });
+                
+            }
+            return employee;
+        }
+    }
+}
---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkInsert.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
